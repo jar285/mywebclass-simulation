edf8cec8025f6254dd7801a13da3109842adc223
Greys Espinoza <gle3@njit.edu>
2023-03-25 15:14:06 -0400

    add test for WCAG and W3C validation-1

--- tests/mywebclass_validation.spec.js
+++ tests/mywebclass_validation.spec.js
@@ -1,13 +1,17 @@
 const { test, expect } = require('@playwright/test');
+const HTML5Validator = require('html5-validator');
 
-test('HTML Validation Test', async ({ page }) => {
+test('HTML Validation Test', async ({ context, page }) => {
+  // Add the html5-validator library to the page context
+  await context.addInitScript({ content: `window.HTML5Validator = ${HTML5Validator.toString()}` });
+
   // Navigate to the website to test
   await page.goto('http://localhost:3000');
 
   // Check if there are any HTML validation errors
   const validationErrors = await page.evaluate(() => {
     const validationErrors = [];
-    const validator = new window.html5Validator();
+    const validator = new window.HTML5Validator();
 
     validator.check(document, (err) => {
       if (err) {
