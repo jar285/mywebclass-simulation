9e8b88c2000125541eccaf5d284860d575ada161
Anneryshc <afh9@njit.edu>
2023-03-26 21:35:34 -0400

    Test the time automated.

--- tests/mywebclass_measures.spec.js
+++ tests/mywebclass_measures.spec.js
@@ -1,25 +1,15 @@
+// @ts-check
 const { test, expect } = require('@playwright/test')
 
-test('Page load speed test with static search term', async ({ page }) => {
-  // Establecer un término de búsqueda estático
-  const searchTerm = 'prueba';
-
+test('Page load speed test', async ({ page }) => {
   // Navigate to the page you want to test
   await page.goto('http://localhost:3000')
 
-  // Fill in the search input with the static search term and measure the time it takes for the page to fully load
-  const pageLoadTime = await page.evaluate((searchTerm) => {
+  // Measure the time it takes for the page to fully load
+  const pageLoadTime = await page.evaluate(() => {
     const timing = performance.timing
-    const input = document.querySelector('input[type="search"]');
-    input.value = searchTerm;
-    return new Promise(resolve => {
-      input.addEventListener('input', () => {
-        if (timing.loadEventEnd > 0) {
-          resolve(timing.loadEventEnd - timing.navigationStart);
-        }
-      });
-    });
-  }, searchTerm)
+    return timing.loadEventEnd - timing.navigationStart
+  })
 
   // Expect the page to load in under 3 seconds (3000 ms)
   expect(pageLoadTime).toBeLessThanOrEqual(3000)
