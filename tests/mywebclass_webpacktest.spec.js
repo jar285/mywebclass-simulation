af4c50f696bd423f7acf938e9b6a1cabcb90cf84
Greys Espinoza <gle3@njit.edu>
2023-03-27 18:32:38 -0400

    test for webpack-1

--- tests/mywebclass_webpacktest.spec.js
+++ tests/mywebclass_webpacktest.spec.js
@@ -1,15 +1,13 @@
+// @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('Webpack builds successfully', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  const result = await page.evaluate(() => {
-    return new Promise(resolve => {
-      const script = document.createElement('script');
-      script.onload = () => resolve('Webpack build successful!');
-      script.onerror = () => resolve('Webpack build failed.');
-      script.src = 'http://localhost:3000/js/app.bundle.js';
-      document.body.appendChild(script);
-    });
+test.describe('Webpack Build Status Test', () => {
+  test('Webpack builds successfully', async ({ page }) => {
+    // Navigate to the page that displays the build status
+    await page.goto('http://localhost:3000/webpack-build-status');
+
+    // Verify that the build status is displayed as expected
+    const buildStatus = await page.innerText('#build-status');
+    expect(buildStatus).toBe('Webpack build successful!');
   });
-  expect(result).toBe('Webpack build successful!');
 });
