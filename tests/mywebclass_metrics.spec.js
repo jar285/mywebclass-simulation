afe8a05198a643973c2cb1e63cdf5d59b08cd45f
jar285 <jar285@njit.edu>
2023-03-27 17:26:14 -0400

    Testing quality assurance and metrics

--- tests/mywebclass_metrics.spec.js
+++ tests/mywebclass_metrics.spec.js
@@ -1,17 +1,37 @@
-const { chromium } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-(async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+test('MyWebClass.org performance test', async ({ page }) => {
+  // Set the network and page metrics to gather
+  await page.route('**/*', route => {
+    const request = route.request();
+    route.continue();
+    if (request.url().startsWith('http')) {
+      console.log(request.url(), route.metrics());
+    }
+  });
 
-  // Navigate to the mywebclass.org homepage
+  // Navigate to the website and measure performance
   await page.goto('http://localhost:3000');
+  const loadTime = (await page.metrics()).TaskDuration;
+  const resources = await page.resources();
+  const networkTime = resources.reduce((a, b) => a + b.timing.endTime - b.timing.startTime, 0);
 
-  // Measure site performance
-  const metrics = await page.metrics();
-  console.log(metrics);
+  // Check if the page loads within a certain time limit
+  expect(loadTime).toBeLessThanOrEqual(5000); // 5 seconds
 
-  // Close the browser
-  await browser.close();
-})();
+  // Check for proper resource loading
+  expect(resources).not.toBe([]);
+  for (const resource of resources) {
+    expect(resource.response()).not.toBe(null);
+    expect(resource.request().failure()).toBe(null);
+  }
+
+  // Analyze and optimize site performance based on test results
+  // ...
+
+  // Create test reports with detailed performance data
+  // ...
+
+  // Implement optimizations based on test results
+  // ...
+});
