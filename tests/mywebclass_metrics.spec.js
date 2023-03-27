f17cdfae05d4a0fdc984d8e7bceec967af86b643
jar285 <jar285@njit.edu>
2023-03-27 17:41:38 -0400

    Testing quality assurance and metrics part 3

--- tests/mywebclass_metrics.spec.js
+++ tests/mywebclass_metrics.spec.js
@@ -1,25 +1,34 @@
-async function metrics(page) {
-  const client = await page.context().newCDPSession(page);
-  await client.send('Performance.enable');
+const { test, expect } = require('@playwright/test');
+
+test('MyWebClass.org has alt text on images', async ({ page }) => {
+  // Check for alt text on images
   await page.goto('http://localhost:3000/');
+  const images = await page.$$('img');
+  for (const image of images) {
+    const alt = await image.getAttribute('alt');
+    expect(alt).toBeTruthy();
+  }
+});
 
-  // Wait for page to load completely
-  await page.waitForLoadState('networkidle');
+test('MyWebClass.org has a valid title', async ({ page }) => {
+  // Check for valid title
+  await page.goto('http://localhost:3000/');
+  const title = await page.title();
+  expect(title).toContain('MyWebClass.org');
+});
 
-  // Get performance metrics
-  const performanceMetrics = await client.send('Performance.getMetrics');
+test('MyWebClass.org has a responsive design', async ({ page }) => {
+  // Check for responsive design
+  await page.goto('http://localhost:3000/');
+  const viewport = page.viewportSize();
+  expect(viewport.width).toBeGreaterThan(0);
+  expect(viewport.height).toBeGreaterThan(0);
+});
 
-  // Filter out only relevant metrics
-  const relevantMetrics = performanceMetrics.metrics.filter(metric => {
-    return metric.name === 'ScriptDuration' || metric.name === 'TaskDuration' || metric.name === 'LayoutDuration';
-  });
-
-  // Calculate total time
-  let totalTime = 0;
-  relevantMetrics.forEach(metric => {
-    totalTime += metric.value;
-  });
-
-  // Return result in seconds
-  return totalTime / 1000;
-}
+test('MyWebClass.org has a valid SSL certificate', async ({ page }) => {
+  // Check for valid SSL certificate
+  await page.goto('https://localhost:3000/');
+  const securityDetails = page.securityDetails();
+  expect(securityDetails.protocol()).toBe('TLS 1.2');
+  expect(securityDetails.subjectName()).toContain('mywebclass.org');
+});
