82f7aef1f890fe44e33f94ff0397af12065a152f
jar285 <jar285@njit.edu>
2023-03-27 17:49:21 -0400

    Testing quality assurance and metrics part 5

--- tests/mywebclass_metrics.spec.js
+++ tests/mywebclass_metrics.spec.js
@@ -1,34 +1,35 @@
 const { test, expect } = require('@playwright/test');
 
-test('MyWebClass.org has alt text on images', async ({ page }) => {
-  // Check for alt text on images
-  await page.goto('http://localhost:3000/');
-  const images = await page.$$('img');
-  for (const image of images) {
-    const alt = await image.getAttribute('alt');
-    expect(alt).toBeTruthy();
-  }
-});
+test.describe('MyWebClass.org metrics tests', () => {
+  test.beforeEach(async ({ page }) => {
+    // Navigate to site
+    await page.goto('http://localhost:3000');
+  });
 
-test('MyWebClass.org has a valid title', async ({ page }) => {
-  // Check for valid title
-  await page.goto('http://localhost:3000/');
-  const title = await page.title();
-  expect(title).toContain('MyWebClass.org');
-});
+  test('MyWebClass.org has alt text on images', async ({ page }) => {
+    // Check for alt text on images
+    const images = await page.$$('img');
+    for (const image of images) {
+      const alt = await image.getAttribute('alt');
+      expect(alt).toBeTruthy();
+    }
+  });
 
-test('MyWebClass.org has a responsive design', async ({ page }) => {
-  // Check for responsive design
-  await page.goto('http://localhost:3000/');
-  const viewport = page.viewportSize();
-  expect(viewport.width).toBeGreaterThan(0);
-  expect(viewport.height).toBeGreaterThan(0);
-});
+  test('MyWebClass.org has a valid SSL certificate', async ({ page }) => {
+    // Check for valid SSL certificate
+    const response = await page.goto('http://localhost:3000', { waitUntil: 'domcontentloaded' });
+    const securityDetails = response.securityDetails();
+    expect(securityDetails.protocol()).toBe('TLS 1.2');
+    expect(securityDetails.validFrom()).toBeGreaterThan(0);
+    expect(securityDetails.validTo()).toBeGreaterThan(Date.now() / 1000);
+    expect(securityDetails.issuer()).toContain('Let\'s Encrypt');
+  });
 
-test('MyWebClass.org has a valid SSL certificate', async ({ page }) => {
-  // Check for valid SSL certificate
-  await page.goto('https://localhost:3000/');
-  const securityDetails = page.securityDetails();
-  expect(securityDetails.protocol()).toBe('TLS 1.2');
-  expect(securityDetails.subjectName()).toContain('mywebclass.org');
+  test('MyWebClass.org has expected load time', async ({ page }) => {
+    // Measure load time
+    const startTime = Date.now();
+    await page.goto('http://localhost:3000', { waitUntil: 'load' });
+    const loadTime = Date.now() - startTime;
+    expect(loadTime).toBeLessThanOrEqual(5000); // 5 seconds
+  });
 });
