f37f5b67b9fe156d25eeb24556ec236be0fba388
Anneryshc <afh9@njit.edu>
2023-03-26 22:27:58 -0400

    Add test the  disagree button the cookies and privacy.

--- tests/mywebclass_disagreeBtn.spec.js
+++ tests/mywebclass_disagreeBtn.spec.js
@@ -1,6 +1,7 @@
+// @ts-check
 const { test, expect } = require('@playwright/test')
 
-test('Website should not use cookies after user disagrees', async ({ page }) => {
+test('Website should use cookies after user disagree', async ({ page }) => {
   // Navigate to your website
   await page.goto('http://localhost:3000')
 
@@ -8,17 +9,17 @@ test('Website should not use cookies after user disagrees', async ({ page }) => 
   const popup = await page.$('.modal-content')
   expect(popup).not.toBeNull()
 
-  // Click on the Disagree button
+  // Click on the Agree button
   const disagreeButton = await popup.$('#disagreeButton')
   expect(disagreeButton).not.toBeNull()
   await disagreeButton.click()
 
-  // Wait for the cookie to be removed
+  // Wait for the cookie to be set
   await page.waitForTimeout(1000)
 
   // Check if cookies are being used
   const cookies = await page.evaluate(() => {
     return document.cookie
   })
-  expect(cookies).toBe('')
+  expect(cookies).toBeDefined()
 })
