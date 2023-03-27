8b10a5caca5a897d241398d667cfa5d09f522544
jar285 <jar285@njit.edu>
2023-03-27 17:57:36 -0400

    Testing quality assurance and metrics part 6

--- tests/mywebclass_metrics.spec.js
+++ tests/mywebclass_metrics.spec.js
@@ -1,35 +1,25 @@
 const { test, expect } = require('@playwright/test');
 
-test.describe('MyWebClass.org metrics tests', () => {
-  test.beforeEach(async ({ page }) => {
-    // Navigate to site
-    await page.goto('http://localhost:3000');
+test('MyWebClass.org has a valid SSL certificate', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  const securityDetails = await page.context().securityDetails();
+  expect(securityDetails.protocol()).toBe('TLS 1.3');
+});
+
+test.describe('CI/CD Pipeline - Performance Test', () => {
+  let page;
+
+  test.beforeEach(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
   });
 
-  test('MyWebClass.org has alt text on images', async ({ page }) => {
-    // Check for alt text on images
-    const images = await page.$$('img');
-    for (const image of images) {
-      const alt = await image.getAttribute('alt');
-      expect(alt).toBeTruthy();
-    }
+  test.afterEach(async () => {
+    await page.close();
   });
 
-  test('MyWebClass.org has a valid SSL certificate', async ({ page }) => {
-    // Check for valid SSL certificate
-    const response = await page.goto('http://localhost:3000', { waitUntil: 'domcontentloaded' });
-    const securityDetails = response.securityDetails();
-    expect(securityDetails.protocol()).toBe('TLS 1.2');
-    expect(securityDetails.validFrom()).toBeGreaterThan(0);
-    expect(securityDetails.validTo()).toBeGreaterThan(Date.now() / 1000);
-    expect(securityDetails.issuer()).toContain('Let\'s Encrypt');
-  });
-
-  test('MyWebClass.org has expected load time', async ({ page }) => {
-    // Measure load time
-    const startTime = Date.now();
-    await page.goto('http://localhost:3000', { waitUntil: 'load' });
-    const loadTime = Date.now() - startTime;
-    expect(loadTime).toBeLessThanOrEqual(5000); // 5 seconds
+  test('Load Time', async () => {
+    const loadTime = await page.evaluate(() => window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart);
+    expect(loadTime).toBeLessThanOrEqual(5000);
   });
 });
