1f69a56d0a4ad47b35aa3af8093fc7f3d1692952
jar285 <jar285@njit.edu>
2023-03-27 18:07:56 -0400

    Testing quality assurance metrics part 7

--- tests/mywebclass_metrics.spec.js
+++ tests/mywebclass_metrics.spec.js
@@ -1,25 +1,37 @@
-const { test, expect } = require('@playwright/test');
+const { chromium } = require('@playwright/test');
 
-test('MyWebClass.org has a valid SSL certificate', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  const securityDetails = await page.context().securityDetails();
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  const navigationPromise = page.waitForNavigation();
+
+  // Load the website
+  await page.goto('https://mywebclass.org');
+
+  // Check that the website has a valid SSL certificate
+  const securityDetails = await page.evaluate(() => JSON.parse(JSON.stringify(window.performance.getEntriesByType("navigation")[0]['securityDetails'])));
   expect(securityDetails.protocol()).toBe('TLS 1.3');
-});
 
-test.describe('CI/CD Pipeline - Performance Test', () => {
-  let page;
+  // Check that the website has no broken links
+  const links = await page.$$eval('a', links => links.map(link => link.href));
+  const responses = await Promise.all(links.map(link => page.goto(link, {waitUntil: 'domcontentloaded'}).catch(e => e)));
+  const errorResponses = responses.filter(response => response instanceof Error);
+  expect(errorResponses.length).toBe(0);
 
-  test.beforeEach(async ({ browser }) => {
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-  });
+  // Check that the website has appropriate meta tags
+  const title = await page.title();
+  expect(title).toBeTruthy();
 
-  test.afterEach(async () => {
-    await page.close();
-  });
+  const description = await page.$eval('meta[name="description"]', el => el.content);
+  expect(description).toBeTruthy();
 
-  test('Load Time', async () => {
-    const loadTime = await page.evaluate(() => window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart);
-    expect(loadTime).toBeLessThanOrEqual(5000);
-  });
-});
+  // Simulate interacting with the site
+  // ...
+
+  // Collect metrics
+  const metrics = await page.metrics();
+  console.log(metrics);
+
+  // Close the browser
+  await browser.close();
+})();
