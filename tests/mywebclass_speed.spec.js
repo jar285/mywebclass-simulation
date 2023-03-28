91730f4703cd395a09098f4c84747a6c19fd1083
jar285 <jar285@njit.edu>
2023-03-27 23:08:33 -0400

    Website perfomance

--- tests/mywebclass_speed.spec.js
+++ tests/mywebclass_speed.spec.js
@@ -1,25 +1,15 @@
-const { test, expect } = require('@playwright/test');
+// @ts-check
+const { test, expect } = require('@playwright/test')
 
-const WEBSITE_URL = 'http://localhost:3000';
+test('Performance Test', async ({ page }) => {
+  // Navigate to the website to test
+  await page.goto('http://localhost:3000')
 
-test.describe('Website Performance Test', () => {
-  let page;
+  // Measure the load time of the website
+  const loadStartTime = Date.now()
+  await page.waitForLoadState('networkidle')
+  const loadTime = Date.now() - loadStartTime
 
-  test.beforeEach(async ({ browser }) => {
-    page = await browser.newPage();
-    await page.goto(WEBSITE_URL, { waitUntil: 'domcontentloaded' });
-  });
-
-  test.afterEach(async () => {
-    await page.close();
-  });
-
-  test('Should load the website within 3 seconds', async () => {
-    const navigationPromise = page.waitForNavigation();
-    const startTime = Date.now();
-    await page.goto(WEBSITE_URL, { waitUntil: 'domcontentloaded' });
-    await navigationPromise;
-    const endTime = Date.now();
-    const loadTime = endTime - startTime;
-    expect(loadTime).toBeLessThanOrEqual(3000);
-  });
+  // Check if the load time is below a certain threshold
+  expect(loadTime).toBeLessThanOrEqual(5000) // 5 seconds
+})
