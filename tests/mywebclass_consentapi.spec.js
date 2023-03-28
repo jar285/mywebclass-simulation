12f3f0bfffc5bf3414e0edc7a829eb77bf2ad63e
jar285 <jar285@njit.edu>
2023-03-28 02:31:22 -0400

    Consent API Google analytics

--- tests/mywebclass_consentapi.spec.js
+++ tests/mywebclass_consentapi.spec.js
@@ -1,30 +1,11 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
-test('Google Analytics Consent API should be implemented', async ({ context }) => {
-  const page = await context.newPage();
-  await page.goto('http://localhost:3000'); // Replace with your website URL
+test('Google analytics consent API should be implemented', async ({ context }) => {
+  const page = await context.newPage()
+  await page.goto('http://localhost:3000') // Replace with your website URL
 
-  // Check if the Google Analytics Consent API is loaded
-  const isGoogleAnalyticsLoaded = await page.evaluate(() => {
-    return typeof ga === 'function' && typeof gaOptout === 'function';
-  });
+  const cookies = await context.cookies()
+  const hasGoogleAnalyticsCookie = cookies.some(cookie => cookie.name === '_ga')
 
-  expect(isGoogleAnalyticsLoaded).toBeTruthy();
-
-  // Check if the Google Analytics cookie is blocked until the user gives consent
-  const cookiesBeforeConsent = await context.cookies();
-  expect(cookiesBeforeConsent.some(cookie => cookie.name === '_ga')).toBeFalsy();
-
-  // Give consent to Google Analytics tracking
-  await page.click('#agreeButton');
-
-  // Check if the Google Analytics cookie is set after the user gives consent
-  const cookiesAfterConsent = await context.cookies();
-  expect(cookiesAfterConsent.some(cookie => cookie.name === '_ga')).toBeTruthy();
-
-  // Check if the Google Analytics cookie is blocked after the user opts out of tracking
-  await page.click('#disagreeButton');
-  const cookiesAfterOptOut = await context.cookies();
-  expect(cookiesAfterOptOut.some(cookie => cookie.name === '_ga')).toBeFalsy();
-});
+  expect(hasGoogleAnalyticsCookie).toBeTruthy()
+})
