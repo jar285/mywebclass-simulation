732f06530a04c157cc7f8b31bc92270530da2948
Greys Espinoza <gle3@njit.edu>
2023-03-28 02:14:07 -0400

    test for keyboard nav

--- tests/mywebclass_keyboardtest.spec.js
+++ tests/mywebclass_keyboardtest.spec.js
@@ -3,20 +3,21 @@ const { test, expect } = require('@playwright/test');
 
 test('Keyboard navigation test', async ({ page }) => {
   // Navigate to the page you want to test
-  await page.goto('http://localhost:3000/home.html');
-
-  // Find the button element
-  const button = await page.$('#my-button');
+  await page.goto('http://localhost:3000');
 
   // Check if button exists before attempting to focus on it
+  const button = await page.$('#my-button');
   expect(button).not.toBeNull();
 
   // Focus on the button and press Enter
   await button.focus();
   await page.keyboard.press('Enter');
 
-  // Check that the expected element is now visible
+  // Wait for the message to become visible
   const message = await page.$('#my-message');
+  await message.waitForElementState('visible');
+
+  // Check that the expected element is now visible
   expect(await message.isVisible()).toBe(true);
 });
 
